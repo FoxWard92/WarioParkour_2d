fe0bf64e494b8681bdcc15857429b825796d85d3
FoxMine92 <foxwardmine92@gmail.com>
2024-11-22 16:48:54 +0100

    asd

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-s
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAcAKZvsgp_iIGBGluUaDPSmxQJOGaFTfk",
+
+    authDomain: "wario-parkour.firebaseapp.com",
+
+    databaseURL: "https://wario-parkour-default-rtdb.firebaseio.com",
+
+    projectId: "wario-parkour",
+
+    storageBucket: "wario-parkour.firebasestorage.app",
+
+    messagingSenderId: "693490415402",
+
+    appId: "1:693490415402:web:cf30b1e1698306c1b6d053",
+
+    measurementId: "G-V099PR2HXK"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
