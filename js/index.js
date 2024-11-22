89441c80c6e50b57ecc529d2f0b76773d8ff4f5c
FoxMine92 <foxwardmine92@gmail.com>
2024-11-22 16:59:27 +0100

    asd

--- js/index.js
+++ js/index.js
@@ -5,19 +5,12 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase
 const firebaseConfig = {
 
     apiKey: "AIzaSyAcAKZvsgp_iIGBGluUaDPSmxQJOGaFTfk",
-
     authDomain: "wario-parkour.firebaseapp.com",
-
     databaseURL: "https://wario-parkour-default-rtdb.firebaseio.com",
-
     projectId: "wario-parkour",
-
     storageBucket: "wario-parkour.firebasestorage.app",
-
     messagingSenderId: "693490415402",
-
     appId: "1:693490415402:web:cf30b1e1698306c1b6d053",
-
     measurementId: "G-V099PR2HXK"
 
 };
